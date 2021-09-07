3d83880f0a619abe09fa0efa2f2ca43dd4d48c12
jijihadi <jijihadi0123@gmail.com>
2021-09-08 00:32:15 +0700

    dashboard admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Hay user, {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
