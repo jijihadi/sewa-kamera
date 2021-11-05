760b21decff5f4fd099edb4f9e23710735004147
jijihadi <50286299+jijihadi@users.noreply.github.com>
2021-11-06 02:01:39 +0700

    hilangkaaan semua ghosting

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -307,7 +307,7 @@
 
             <!-- partial:partials/_footer.html -->
             <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-                <p class="text-warning text-center"> Full paid to remove this karna saya trauma dighosting</p>
+                {{-- <p class="text-warning text-center"> Full paid to remove this karna saya trauma dighosting</p> --}}
                 <p class="text-muted text-center text-md-left">Copyright © {{date('Y')}} <a href="https://www.nobleui.com"
                         target="_blank">Aros</a>. All rights reserved</p>
                 <p class="text-muted text-center text-md-left mb-0 d-none d-md-block">Handcrafted With <i
